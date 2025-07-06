9ef039e88289b3ab361ecdc338b257c2fa0efd7d
cbe <cbe@users.noreply.github.com>
2025-07-06 15:21:18 +0200

    Extract some code to keep keymap clean

---
+++ keyboards/crkbd/keymaps/crbrd/combos.c
@@ -0,0 +1,32 @@
+/*
+Copyright 2025 @cbe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+#ifdef COMBO_ENABLE
+    const uint16_t PROGMEM toggle_cw[]     = {KC_W, HD2_LRA, COMBO_END};
+    const uint16_t PROGMEM num_arrow[]     = {KC_4, KC_EQL , COMBO_END};
+    const uint16_t PROGMEM num_colon[]     = {KC_4, KC_ASTR, COMBO_END};
+    const uint16_t PROGMEM num_backspace[] = {KC_4, KC_0   , COMBO_END};
+
+    combo_t key_combos[] = {
+        COMBO(toggle_cw    , TOGGLE_CAPS_WORD),
+        COMBO(num_arrow    , KC_GT  ),
+        COMBO(num_colon    , KC_SCLN),
+        COMBO(num_backspace, KC_BSPC),
+    };
+#endif
---
+++ keyboards/crkbd/keymaps/crbrd/flow_tap.c
@@ -0,0 +1,38 @@
+/*
+Copyright 2025 @cbe
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+// Flow tap per key
+// See: https://docs.qmk.fm/tap_hold#is-flow-tap-key
+bool is_flow_tap_key(uint16_t keycode) {
+    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
+        return false; // Disable Flow Tap on hotkeys.
+    }
+
+    switch (get_tap_keycode(keycode)) {
+        case KC_A ... KC_E: // Exclude 'KC_F' as it's required for umlauts (via 'RALT_T', see 'HD2_LRA')
+        case KC_G ... KC_Z:
+        case KC_DOT:
+        case KC_COMM:
+        case KC_SCLN:
+        case KC_SLSH:
+            return true;
+    }
+
+    return false;
+}
